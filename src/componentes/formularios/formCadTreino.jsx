558848ea7a810c9ffb126a59d9655e2efeefc5e7
gcaveroldi <gcaaveroldi@gmail.com>
2024-03-24 14:40:49 -0300

    feats: correções categorias

--- src/componentes/formularios/formCadTreino.jsx
+++ src/componentes/formularios/formCadTreino.jsx
@@ -1,19 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import { useState } from 'react';
 
-export default function FormCadTreino(props){
+export default function FormCadTreino(props) {
     const [validado, setValidado] = useState(false);
+    const [clienteTreino, setClienteTreino] = useState([{
+        codigo: 0,
+        nome: "Nenhum Treino encontrado"
+    }]);
     const [treino, setTreino] = useState({
         codigo: "",
-        categoria:{}
+        categoria: ""
     });
 
+    useEffect(() => {
+        buscaTreino();
+    }, []);
+
+    function buscaTreino() {
+        fetch("http://localhost:4000/treino", { method: "GET" })
+            .then(resposta => resposta.json())
+            .then(retorno => {
+                if (retorno.status) {
+                    setClienteTreino(retorno.listaTreino);
+                }
+            })
+            .catch(erro => {
+                setClienteTreino([{
+                    codigo: 0,
+                    nome: "Nenhum Treino encontrado: " + erro.message
+                }]);
+            });
+    }
+
+    useEffect(()=>{
+        buscaTreino();
+    }, [])
+
     function manipularMudanca(evento) {
-        const componente = evento.currentTarget;
-        setTreino({ ...treino, [componente.name]: componente.value });
+        const { name, value } = evento.target;
+        setTreino({ ...treino, [name]: value });
     }
 
     function manipularSubmissao(evento) {
@@ -22,13 +50,12 @@ export default function FormCadTreino(props){
         const form = evento.currentTarget;
         if (form.checkValidity() === false) {
             setValidado(true);
-        }
-        else{
+        } else {
             setValidado(false);
             props.setExibirTabela(true);
-            
         }
     }
+
     return (
         <Form noValidate validated={validado} onSubmit={manipularSubmissao}>
             <Row className="mb-3">
@@ -49,19 +76,22 @@ export default function FormCadTreino(props){
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} md="4">
-                    <Form.Label>Categoria:</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="0"
-                        value={treino.codigo}
-                        id="categoria"
-                        name="categoria"
-                        onChange={manipularMudanca}
-                        disabled
-                    />
+                <Form.Label>Categoria:</Form.Label>
+                    <Form.Select id="categoria" name="categoria">
+                    {
+                        clienteTreino.map((clienteTreino) => {
+                            return (
+                                <option key={clienteTreino.codigo} value={clienteTreino.codigo}>
+                                    {clienteTreino.descricao}
+                                </option>
+                            );
+
+                            })
+                         
+                        }
+                        
+                    </Form.Select>
                     
-                    <Form.Control.Feedback type='invalid'>Por favor, informe a categoria do treino.</Form.Control.Feedback>
                 </Form.Group>
             </Row>
             <Button type="submit">Gravar</Button>
